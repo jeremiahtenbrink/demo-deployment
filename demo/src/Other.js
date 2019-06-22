de6c91e6a6be96902b83b405dd9de7f6c3876951
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-06-21 18:26:26 -0600

    added additional route

---
+++ demo/src/Other.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Other = props => {
+    return ( <div>
+        <h1>Other</h1>
+        </div> );
+};
+
+Other.propTypes = {
+
+};
+
+export default Other;
