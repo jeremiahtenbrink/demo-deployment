90d9630db130e8fdc298210b997533769d1b12b1
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-06-21 18:44:35 -0600

    added additionial dep

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,11 +6,11 @@ import Other from "./Other";
 
 function App(){
     return ( <div className="App">
-            <header className="App-header">
-                <h1>Index</h1>
-                <Route path={ "/other" } component={ Other }/>
-            </header>
-        </div> );
+        <header className="App-header">
+            <h1>Index</h1>
+            <Route path={ "other" } component={ Other }/>
+        </header>
+    </div> );
 }
 
 export default App;
